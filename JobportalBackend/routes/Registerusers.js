5b1ac23e43b63c4fc09d6d0a47da0511d53755ba
Uthum Wijenayake <uthumwijenayake@gmail.com>
2025-03-16 18:06:12 +0530

    jwt token added

--- JobportalBackend/routes/Registerusers.js
+++ JobportalBackend/routes/Registerusers.js
@@ -1,57 +1,70 @@
-const router=require('express').Router();
+const router = require('express').Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 let Registeruser = require('../models/Registeruser');
 
-router.route("/add").post((req,res)=>{
+router.route("/add").post(async (req, res) => {
     const userName = req.body.userName;
     const email = req.body.email;
     const password = req.body.password;
     const confirmPassword = req.body.confirmPassword;
 
+    // Check if user already exists
+    const existingUser = await Registeruser.findOne({ email });
+
+    if (existingUser) return res.status(400).json({ message: "User already exists" });
+
+    // Hash the password
+    const salt = bcrypt.genSalt(10);
+    const hashedPassword = bcrypt.hash(password, salt);
+
+    //create new user
     const newRegisteruser = new Registeruser({
         userName,
         email,
-        password,
+        password: hashedPassword,
         confirmPassword
     });
 
     newRegisteruser.save()
-    .then(()=>res.json('User added!'))
-    .catch(err=>res.status(400).json('Error: '+err));
+        .then(() => res.json('User added!'))
+        .catch(err => res.status(400).json('Error: ' + err));
 });
 
-router.route("/").get((req,res)=>{
+
+router.route("/").get((req, res) => {
     Registeruser.find()
-    .then(registerusers=>res.json(registerusers))
-    .catch(err=>res.status(400).json('Error: '+err));
+        .then(registerusers => res.json(registerusers))
+        .catch(err => res.status(400).json('Error: ' + err));
 });
 
-router.route("/update/:id").put(async(req,res)=>{
+router.route("/update/:id").put(async (req, res) => {
     let userId = req.params.id;
-    const {userName,email,password,confirmPassword} = req.body;
+    const { userName, email, password, confirmPassword } = req.body;
 
     const updateRegisteruser = {
         userName,
         email,
         password,
-        confirmPassword 
+        confirmPassword
     };
 
-    const update = await Registeruser.findByIdAndUpdate(userId,updateRegisteruser).then(()=>{
-        res.status(200).send({status: "User updated"});
-    }).catch(err=>{
+    const update = await Registeruser.findByIdAndUpdate(userId, updateRegisteruser).then(() => {
+        res.status(200).send({ status: "User updated" });
+    }).catch(err => {
         console.log(err);
-        res.status(500).send({status: "Error with updating data", error: err.message});
-    });    
+        res.status(500).send({ status: "Error with updating data", error: err.message });
+    });
 });
 
-router.route("/delete/:id").delete(async(req,res)=>{
+router.route("/delete/:id").delete(async (req, res) => {
     let userId = req.params.id;
 
-    await Registeruser.findByIdAndDelete(userId).then(()=>{
-        res.status(200).send({status: "User deleted"});
-    }).catch(err=>{
+    await Registeruser.findByIdAndDelete(userId).then(() => {
+        res.status(200).send({ status: "User deleted" });
+    }).catch(err => {
         console.log(err);
-        res.status(500).send({status: "Error with deleting data", error: err.message});
+        res.status(500).send({ status: "Error with deleting data", error: err.message });
     });
 });
 
