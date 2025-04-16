1fb7f6c4a12ad45965dfe641dc002abf21a60342
DinushaHerath <dinushaherath390@gmail.com>
2025-04-16 21:25:45 +0530

    backend of chat signup module

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,9 +1,19 @@
 import express from "express";
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
+
+dotenv.config(); // <-- Load environment variables
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.use("/api/auth",authRoutes)
+app.use(express.json())
 
-app.listen(5001, ()=> {
-    console.log("server is running on port 5001")
-})
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT: ${PORT}`);
+    connectDB();
+});
