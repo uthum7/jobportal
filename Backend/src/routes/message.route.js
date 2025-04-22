1c8788a817d793ccb65949f37d50f39c631ad56d
AwarjanaC <hansichan.0916@gmail.com>
2025-04-22 21:47:40 +0530

    Making Changes

--- Backend/src/routes/message.route.js
+++ Backend/src/routes/message.route.js
@@ -1,5 +1,6 @@
 import express from "express";
 import {protectRoute} from "../middleware/auth.middleware.js";
+import {sendMessage, getMessages, getUserForSidebar} from "../controllers/message.controllers.js"
 
 const router = express.Router();
 
