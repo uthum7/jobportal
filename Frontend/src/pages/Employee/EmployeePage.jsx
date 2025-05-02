27fe87eec09203b2f6f46701f925af59d08f5b64
AwarjanaC <hansichan.0916@gmail.com>
2025-05-02 22:45:46 +0530

    Modification 02

--- Frontend/src/pages/Employee/EmployeePage.jsx
+++ Frontend/src/pages/Employee/EmployeePage.jsx
@@ -1,14 +1,36 @@
 import React from 'react'
 import "./employee-page.css"
+import EmployeeSidebar from '../../components/Employee/Sidebar/EmployeeSidebar'
 const EmployeePage = () => {
   return (
     <div className="employee-page">
       <div className="row">
         <div className="sidebar">
-
+            <EmployeeSidebar/>
         </div>
         <div className="middle">
-
+          <p>Hello World</p>
+          <p>Hello World</p>
+          <p>Hello World</p>
+          <p>Hello World</p>
+          <p>Hello World</p>
+          <p>Hello World</p>
+          <p>Hello World</p>
+          <p>Hello World</p>
+          <p>Hello World</p>
+          <p>Hello World</p>
+          <p>Hello World</p>
+          <p>Hello World</p>
+          <p>Hello World</p>
+          <p>Hello World</p>
+          <p>Hello World</p>
+          <p>Hello World</p>
+          <p>Hello World</p>
+          <p>Hello World</p>
+          <p>Hello World</p>
+          <p>Hello World</p>
+          <p>Hello World</p>
+          <p>Hello World</p>
         </div>
       </div>
     </div>
